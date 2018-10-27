fcedc4b21b3dcff28500cb8f7e01f3fa45c2b279
Chris Woodle <chris@droplit.io>
2018-10-27 17:15:52 -0500

    added body parser

--- crm-server/server.js
+++ crm-server/server.js
@@ -1,7 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const bodyParser = require('body-parser');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/account', (req, res) => {
+    res.send('Hello World!')
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
